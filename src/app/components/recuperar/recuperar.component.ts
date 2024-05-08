075ec0c81216f587127675c5446d9d3497ef9d4e
Amohmade <mhassouba26@gmail.com>
2024-05-08 17:51:18 +0200

    actualizado

--- src/app/components/recuperar/recuperar.component.ts
+++ src/app/components/recuperar/recuperar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormGroup,FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recuperar',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatFormFieldModule,ReactiveFormsModule,MatInputModule,MatIconModule,MatButtonModule,RouterModule],
   templateUrl: './recuperar.component.html',
   styleUrl: './recuperar.component.css'
 })
 export class RecuperarComponent {
-
+  recuperarForm = new FormGroup({
+    datoRecuperar: new FormControl<string>('',[
+      // Validators.minLength(3),
+      // Validators.required
+    ])
+  });
+  enviar(){
+    console.log(this.recuperarForm.value);
+  }
 }
